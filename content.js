6cab515fa13a891b5d44dcea9636eb0623a11adf
olariuromeo <olariu_romeo@yahoo.it>
2023-12-09 14:35:38 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -8,26 +8,48 @@ console.log("Content script executed on the specified domains.");
 
 // Example: Change the background color of the body to red on www.coozila.com
 if (window.location.hostname === "www.coozila.com") {
-  document.body.style.backgroundColor = "red";
+  document.body.style.backgroundColor = "transparent";
   console.log("Welcome to Coozila! This is the red version.");
 }
 
 // Example: Change the background color of the body to blue on www.coozila.co.uk
 if (window.location.hostname === "www.coozila.co.uk") {
-  document.body.style.backgroundColor = "blue";
+  document.body.style.backgroundColor = "transparent";
   console.log("Welcome to Coozila UK! This is the blue version.");
 }
 
 // Example: Change the background color of the body to green on www.coozila.it
 if (window.location.hostname === "www.coozila.it") {
-  document.body.style.backgroundColor = "green";
+  document.body.style.backgroundColor = "transparent";
   console.log("Benvenuto su Coozila Italia! This is the green version.");
 }
 
 // Example: Change the background color of the body to yellow on www.coozila.ro
 if (window.location.hostname === "www.coozila.ro") {
-  document.body.style.backgroundColor = "yellow";
+  document.body.style.backgroundColor = "transparent";
   console.log("Bine ați venit la Coozila Romania! This is the yellow version.");
 }
 
-// Add similar logic for other domains as needed
+chrome.runtime.sendMessage({ action: "checkNewTabPage" }, (response) => {
+  if (response && response.isNewTabPage) {
+    // Sunteți pe pagina noului tab, adăugați caseta de căutare
+    const searchContainer = document.createElement("div");
+    searchContainer.style.position = "fixed";
+    searchContainer.style.top = "50%";
+    searchContainer.style.left = "50%";
+    searchContainer.style.transform = "translate(-50%, -50%)";
+    searchContainer.style.zIndex = "9999";
+    searchContainer.innerHTML = `
+      <form method="post" action="https://www.coozila.com/">
+        <input type="text" name="q" />
+        <input type="hidden" name="categories" value="general,social media" />
+        <input type="hidden" name="lang" value="all" />
+        <input type="hidden" name="locale" value="en" />
+        <input type="hidden" name="time_range" value="month" />
+      </form>
+    `;
+    
+    document.body.appendChild(searchContainer);
+  }
+});
+
--- service_worker.js
+++ service_worker.js
@@ -3,6 +3,16 @@
 // Ascultați evenimentul de instalare
 self.addEventListener('install', (event) => {
   console.log('Service worker installed:', event);
+
+  // Adaugă o instrucțiune pentru a deschide `newtab.html` la instalare
+  event.waitUntil(
+    clients.claim()  // Preia controlul asupra tab-urilor existente
+      .then(() => self.skipWaiting())  // Așteaptă ca service worker-ul să devină activ imediat
+      .then(() => {
+        // Deschide `newtab.html`
+        return clients.openWindow('newtab.html');
+      })
+  );
 });
 
 // Ascultați evenimentul de activare
@@ -14,3 +24,14 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('message', (event) => {
   console.log('Message from extension:', event.data);
 });
+
+// background.js (or another background script)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/path/to/your/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
